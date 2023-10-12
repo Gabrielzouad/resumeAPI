8c75c3d91fbfe7e79a0ed79b0bc27795c53faf1c
GabrielZouad <z20gab@gmail.com>
2023-10-12 11:31:26 +0200

    removed frontend from application

---
+++ model/model.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+/**
+ * @typedef Recommendation
+ * @property {string} name - The name of the person who wrote the recommendation.
+ * @property {string} company - The company where the person who wrote the recommendation works.
+ * @property {string} text - The text of the recommendation.
+ * @property {Date} date - The date when the recommendation was created.
+ */
+
+const recommendationSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  company: {
+    type: String,
+    required: true,
+  },
+  text: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Recommendation = mongoose.model('Recommendation', recommendationSchema);
+module.exports = Recommendation;
+
+const projectSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  tags: [String],
+  date: {
+    type: String,
+    required: true,
+  },
+  Image: {
+    data: Buffer,
+    contentType: String,
+  },
+});
+
+const Project = mongoose.model('Project', projectSchema);
+module.exports = Project;
